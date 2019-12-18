901d024c9495703d3867e361c29ede614832a990
1938917626@qq.com <1938917626@qq.com>
2019-12-18 15:30:22 +0800

    留言板非空项解决

--- ShoppingCity/ShoppingCity/MessageManage.aspx.cs
+++ ShoppingCity/ShoppingCity/MessageManage.aspx.cs
@@ -4,7 +4,6 @@ namespace ShoppingCity
 {
     public partial class MessageManage : System.Web.UI.Page
     {
-        //主题不能为空,内容不能为空
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["uName"] == null)
@@ -15,15 +14,23 @@ namespace ShoppingCity
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
-            BBSDataContext lq = new BBSDataContext();
-            BBSNote note = new BBSNote();
-            note.bnSubject = txtbnSubject.Text;
-            note.bnContent = txtbnContent.Text;
-            note.bnID = Convert.ToInt16(Session["uID"].ToString());
-            note.bnAddTime = System.DateTime.Now;
-            lq.BBSNote.InsertOnSubmit(note);
-            lq.SubmitChanges();
-            Response.Redirect("MessageManage.aspx");
+            if (txtbnSubject.Text == "" || txtbnContent.Text == "")
+            {
+                ClientScript.RegisterStartupScript(GetType(), "", "<script>alert('主题不能为空,内容不能为空');</script>");
+                return;
+            }
+            else
+            {
+                BBSDataContext lq = new BBSDataContext();
+                BBSNote note = new BBSNote();
+                note.bnSubject = txtbnSubject.Text;
+                note.bnContent = txtbnContent.Text;
+                note.bnID = Convert.ToInt16(Session["uID"].ToString());
+                note.bnAddTime = System.DateTime.Now;
+                lq.BBSNote.InsertOnSubmit(note);
+                lq.SubmitChanges();
+                Response.Redirect("MessageManage.aspx");
+            }
         }
     }
 }
--- ShoppingCity/ShoppingCity/MessageReply.aspx.cs
+++ ShoppingCity/ShoppingCity/MessageReply.aspx.cs
@@ -11,23 +11,31 @@ namespace ShoppingCity
                 ClientScript.RegisterStartupScript(GetType(), "", "<script>alert('请先登录！');location.href='Login.aspx'</script");
             }
 
-            if (Session["id"] == null)
-            {
-                ClientScript.RegisterStartupScript(GetType(), "", "<script>alert('请选择留言主题！');location.href='MessageManage.aspx'</script");
-            }
+            //if (Session["id"] == null)
+            //{
+            //    ClientScript.RegisterStartupScript(GetType(), "", "<script>alert('请选择留言主题！');location.href='MessageManage.aspx'</script>");
+            //}
         }
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
-            BBSDataContext lq = new BBSDataContext();
-            BBSAnswer ba = new BBSAnswer();
-            ba.uID = Convert.ToInt16(Session["uID"]);
-            ba.bnID = Convert.ToInt16(Request["id"]);
-            ba.baContent = txtbaContent.Text;
-            ba.baAddTime = System.DateTime.Now;
-            lq.BBSAnswer.InsertOnSubmit(ba);
-            lq.SubmitChanges();
-            Response.Redirect("MessageReply.aspx?id=" + Request["id"]);
+            if(txtbaContent.Text=="")
+            {
+                ClientScript.RegisterStartupScript(GetType(), "", "<script>alert('留言不能为空！');</script>");
+                return;
+            }
+            else
+            {
+                BBSDataContext lq = new BBSDataContext();
+                BBSAnswer ba = new BBSAnswer();
+                ba.uID = Convert.ToInt16(Session["uID"]);
+                ba.bnID = Convert.ToInt16(Request["id"]);
+                ba.baContent = txtbaContent.Text;
+                ba.baAddTime = System.DateTime.Now;
+                lq.BBSAnswer.InsertOnSubmit(ba);
+                lq.SubmitChanges();
+                Response.Redirect("MessageReply.aspx?id=" + Request["id"]);
+            }
         }
     }
 }
